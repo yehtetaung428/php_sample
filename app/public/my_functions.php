e15bce7b60fb43978d291d1bf08c6e4a2d7cf779
yehtetaung428 <60247684+yehtetaung428@users.noreply.github.com>
2021-04-22 00:03:16 +0900

    docker_php_testing

---
+++ app/public/my_functions.php
@@ -0,0 +1,5 @@
+<?php
+function multiplySelf($var){
+    echo $var;
+}
+?>
---
+++ app/public/variable.php
@@ -0,0 +1,15 @@
+<?php
+// Declaring variables
+$txt = "Hello World!";
+$number = 10;
+
+// Displaying variables value
+echo $txt;
+echo $number;
+echo "<br>";
+
+require "my_functions.php";
+
+multiplySelf(2);
+echo "<br>";
+?>
